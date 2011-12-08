34a5f08082e03f4c46a91dc88633f039282d5d75
zAAmpie <zaampie@gmail.com>
2011-12-08 16:42:35 +0200

    Fixed the mapped file output

--- bucketflushthread.cpp
+++ bucketflushthread.cpp
@@ -25,7 +25,7 @@ void BucketFlushThread::assembleOutputFile(QString tmpfilebase, QString outfile,
     QFile inf;
     QString outfilepart = outfile + ".part";
     QFile outf(outfilepart);
-    if (!outf.open(QIODevice::WriteOnly | QIODevice::Append))
+    if (!outf.open(QIODevice::ReadWrite))
     {
         //TODO: report error
         return;
@@ -42,11 +42,20 @@ void BucketFlushThread::assembleOutputFile(QString tmpfilebase, QString outfile,
         }
         else
         {
-            const char *f = (char*)outf.map(bucket * HASH_BUCKET_SIZE, HASH_BUCKET_SIZE);
-            //QByteArray buf = inf.readAll();
+            QByteArray buf = inf.readAll();
+            if (outf.size() < bucket * HASH_BUCKET_SIZE + buf.size())
+                outf.resize(bucket * HASH_BUCKET_SIZE + buf.size());
+            char *f = (char*)outf.map(bucket * HASH_BUCKET_SIZE, buf.size());
+            
             //outf.write(buf);
-            QByteArray outputmap(QByteArray::fromRawData(f, HASH_BUCKET_SIZE));
-            outputmap = inf.readAll();
+            //QByteArray outputmap(QByteArray::fromRawData(f, HASH_BUCKET_SIZE));
+            //outputmap = inf.readAll();
+
+            if (f != 0)
+                memcpy(f, buf.constData(), buf.size());
+            else
+                qDebug() < "Big booboo for map" << bucket << buf.size();
+
             inf.close();
             inf.remove();
             outf.unmap((unsigned char *)f);
