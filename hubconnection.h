9f42b3c4ae8034271976a541e0d19078aa949420
zAAmpie <zaampie@gmail.com>
2011-12-13 15:00:14 +0200

    Updated licensing

--- hubconnection.h
+++ hubconnection.h
@@ -1,3 +1,20 @@
+/* This file is part of ArpmanetDC. Copyright (C) 2012
+ * Source code can be found at http://code.google.com/p/arpmanetdc/
+ * 
+ * ArpmanetDC is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * ArpmanetDC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with ArpmanetDC.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef HUBCONNECTION_H
 #define HUBCONNECTION_H
 
