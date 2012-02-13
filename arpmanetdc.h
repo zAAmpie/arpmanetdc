6b6a448aadd7ab40982e59206131827bc1ad89d9
zAAmpie <zaampie@gmail.com>
2012-02-13 13:58:45 +0200

    Changed version string to 0.1.6
    - For next release ;)

--- arpmanetdc.h
+++ arpmanetdc.h
@@ -87,7 +87,7 @@ static const QMap<QString, char> PROTOCOL_MAP = initMapValues();
 #define FTP_UPDATE_DIRECTORY "/pub/Windows/Network/DC/ArpmanetDC/"
 #define CHECK_FOR_NEW_VERSION_INTERVAL_MS 1800000 //Every 30min
 
-#define VERSION_STRING "0.1.5"
+#define VERSION_STRING "0.1.6"
 
 //Main GUI window class
 class ArpmanetDC : public QMainWindow
